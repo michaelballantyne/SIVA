97a886d23e1dbe57c7c2effafb21f35d2848884a
Aayush Shrestha - 417903 <ashrestha0011@gmail.com>
2026-06-18 12:00:57 -0600

    init

--- spec.py
+++ spec.py
@@ -2,3 +2,4 @@
 info   = inspect("/vast/projects/autonomousvis/ashrestha/code/VisLang/csafe_heptane_302x302x302_uint8.raw")
 loaded = load(info, dimensions={'grid': 150})   # 302^3 -> ~151^3 for browser responsiveness
 render(loaded, cmap='inferno')   # dark hellfire ramp — black -> blood red -> orange
+#test
